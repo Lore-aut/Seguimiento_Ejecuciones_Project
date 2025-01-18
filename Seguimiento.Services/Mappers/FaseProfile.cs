add8abfb85d119886357953ff6b2eff81ea27e51
Keygoar <keyler.goar@gmail.com>
2025-01-18 01:26:41 -0500

    Creacion del Servicio de Fase y otras configuraciones de la aplicacion Servicio

---
+++ Seguimiento.Services/Mappers/FaseProfile.cs
@@ -0,0 +1,34 @@
+﻿using Seguimiento.domain.Entities.Fases;
+using Seguimiento.GrpcProtos;
+
+using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+
+namespace SeguimientoEjecuciones.Service.Mappers
+{
+    public class FaseProfile: Profile
+    {
+        public FaseProfile()
+        {
+
+            CreateMap<Seguimiento.domain.Entities.Fases.Fase, Seguimiento.GrpcProtos.FaseDTO>()
+                .ForMember(t => t.Name, o => o.MapFrom(s => s.Name.ToString()))
+                .ForMember(t => t.Description, o => o.MapFrom(s => s.Description.ToString()))
+                .ForMember(t => t.Identifier, o => o.MapFrom(s => s.Identifier.ToString()))
+                .ForMember(t => t.Id, o => o.MapFrom(s => s.Id.ToString()))
+                .ForMember(t => t.Items, o => o.MapFrom(s => s.operations));
+
+
+
+            CreateMap<Seguimiento.GrpcProtos.FaseDTO, Seguimiento.domain.Entities.Fases.Fase>()
+                    .ForMember(t => t.Id, o => o.MapFrom(s => new Guid(s.Id)))
+                    .ForMember(t => t.Name, o => o.MapFrom(s => s.Name))
+                    .ForMember(t => t.Description, o => o.MapFrom(s => s.Description))
+                    .ForMember(t => t.Identifier, o => o.MapFrom(s => s.Identifier))
+                    .ForMember(t => t.operations, o => o.MapFrom(s => s.Items));
+            
+            
+
+        }
+    }
+}
---
+++ Seguimiento.Services/Services/FasesService.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using MediatR;
+using Seguimiento.Contracts;
+using Seguimiento.Contracts.Fases;
+using Seguimiento.GrpcProtos;
+using SeguimientoEjecuciones.Application.Fases.Commands.CreateFase;
+using SeguimientoEjecuciones.Application.Fases.Queries.GetFaseById;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using SeguimientoEjecuciones.Service.Mappers;
+using SeguimientoEjecuciones.Application.Fases.Queries.GetAllFases;
+using SeguimientoEjecuciones.Application.Fases.Commands.UpdateFase;
+using SeguimientoEjecuciones.Application.Fases.Commands.DeleteFase;
+
+
+
+namespace SeguimientoEjecuciones.Service.Services
+{
+    public class FasesService : Fase.FaseBase
+    {
+
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public FasesService(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        public override Task<FaseDTO> CreateFase(CreateFaseRequest request, ServerCallContext context)
+        {
+
+            var command = new CreateFaseCommand(
+                request.Name,
+                request.Id,
+                request.Description);
+
+            var result = _mediator.Send(command).Result;
+
+            return Task.FromResult(_mapper.Map<FaseDTO>(result));
+        }
+
+        public override Task<NullableFaseDTO> GetFase(GetRequest request, ServerCallContext context)
+        {
+            var query = new GetFaseByIdQuery(new Guid(request.Id));
+
+            var result = _mediator.Send(query).Result;
+
+            if (result is null)
+                return Task.FromResult(new NullableFaseDTO() { Null = NullValue.NullValue });
+            return Task.FromResult(new NullableFaseDTO() { Fase = _mapper.Map<FaseDTO>(result) });
+        }
+
+        public override Task<Fases> GetAllFases(Empty request, ServerCallContext context)
+        {
+            var query = new GetAllFasesQuery();
+
+            var result = _mediator.Send(query).Result;
+
+            // Convirtiendo de lista de motocicletas al mensaje de lista de DTOs de motocicletas.
+            var FasesDTOs = new Fases();
+            FasesDTOs.Items.AddRange(result.Select(m => _mapper.Map<FaseDTO>(m)));
+
+            return Task.FromResult(FasesDTOs);
+        }
+
+        public override Task<Empty> UpdateFase(FaseDTO request, ServerCallContext context)
+        {
+            var command = new UpdateFaseCommand(_mapper.Map<Seguimiento.domain.Entities.Fases.Fase>(request));
+
+            _mediator.Send(command);
+
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> DeleteFase(DeleteRequest request, ServerCallContext context)
+        {
+            var command = new DeleteFaseCommand(new Guid(request.Id));
+
+            _mediator.Send(command);
+
+            return Task.FromResult(new Empty());
+        }
+
+    }
+}
